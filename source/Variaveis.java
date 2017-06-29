fdab9bd1888f0803f799f08a08bb6c4672702ba7
Murilo <murilo@Murilo.cco.uffs.edu.br>
2017-06-29 15:31:19 -0300

    commit variaveis antes do intervalo

--- source/Variaveis.java
+++ source/Variaveis.java
@@ -23,36 +23,41 @@ public class Variaveis{
 		try{
 			separado = linha.split("#");
 			var = separado[1];
+			System.out.println("var antes "+var);
+
 			separado = var.split("=");
 			var = separado[0];
+			System.out.println("var despois "+var);
 
-			separado = linha.split("#");
-			recebe = separado[1];
-			separado = recebe.split("=");
-			recebe = separado[1];
-			
-			if(!var.equals("")){
-				
-				System.out.println("var "+var);
-				System.out.println("\nrecebe "+(int)recebe.charAt(0));
 
+			if(var.equals("")){
+				System.out.println("Deu pau "+var);
 				return var;
-
-			}else{
+			}else if(!var.equals("")){
+				//if(recebe.)
 				separado = linha.split("#");
-				var = separado[1];
-				System.out.println("var "+var);
-				
+				recebe = separado[1];
+				System.out.println("recebe antes "+recebe);
+
+				separado = recebe.split("=");
+				recebe = separado[1];
+				System.out.println("recebe depois "+recebe);
+					if(!recebe.equals("")){
+						System.out.println("Var: "+var);
+						System.out.println("\nrecebe "+(int)recebe.charAt(0));
+					}else{
+						separado = linha.split("#");
+						var = separado[1];
+						System.out.println("var nada "+var);
+					}
 				return var;
 			}
+
 		}catch(ArrayIndexOutOfBoundsException erro){
 			System.out.println("Syntax Error - "+erro);
 			var="1";
 			return var;
 		}
+		return var;
 	}
-
-
-
-
 }
