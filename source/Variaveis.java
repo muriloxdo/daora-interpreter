9d2e012bf01d74c8bb627c7836199a801ab058e4
muriloxdo <mury_murrilo@hotmail.com>
2017-07-13 14:40:56 -0300

    variaveis arrumado

--- source/Variaveis.java
+++ source/Variaveis.java
@@ -19,7 +19,6 @@ public class Variaveis{
 	public atualiza(nome var, valor)
 	*/
 
-<<<<<<< HEAD
 	public Variaveis(){
 		mapaVariaveis = new TreeMap<String, Integer>();
 	}
@@ -32,31 +31,12 @@ public class Variaveis{
 		String var,operador="x";
 
 		var = exp;
-=======
-    /*
-    public atualiza(nome var, valor)
-    */
-
-    public Variaveis(){
-        mapaVariaveis = new TreeMap<String, Integer>();
-    }
-    
-    //recebe uma string com a expressão aritmética que deve ser resolvida. Ex: "a * b", "1 + b", "2 / 6".
-    public Integer arithmeticExpression( String exp ){
-        
-        String[] separa, splitted;     
-        int op1, op2;
-        String var;
-
-        var = exp;
->>>>>>> c0eac72da8fb38b51c0f6de208f346e952c6dff1
 		separa = var.split("="); //pega a variavel
 		var = separa[0];
 		exp = separa[1];
 		System.out.println(exp);
 
 		splitted = exp.split("[+-/*]"); //splita pelos símbolos possíveis
-<<<<<<< HEAD
 
 		System.out.println(splitted);
 				
@@ -97,73 +77,23 @@ public class Variaveis{
 			
 			System.out.println("resultadoooooo "+result);
 		}
-=======
-
-        System.out.println(splitted);
-				
-        //considerando que a expressão só pode ter dois operandos
-        //você pode aumentar isso
-	
-         //	System.out.println("var="+var);
-        //	System.out.println(mapaVariaveis.keySet());
-	    //	System.out.println("Key: " + mapaVariaveis.get(var) + ". Value: " + mapaVariaveis.get(var));
-
- 		if(mapaVariaveis.containsKey(var)){ //ve se ela existe no map
-        //pega o valor do primeiro operando
-	        try{
-	            op1 = mapaVariaveis.get(splitted[0]);
-
-	        }catch(NullPointerException b){
-
-	            try{
-	                Integer.parseInt(splitted[0]);
-
-	            }catch(NumberFormatException e){
-	                System.out.println("indefined value of"+splitted[0]);              
-	            }
-	        }
-
-	        try{
-	            op2 = mapaVariaveis.get(splitted[1]);
-
-	        }catch(NullPointerException b){
-
-	            try{
-	                Integer.parseInt(splitted[1]);
->>>>>>> c0eac72da8fb38b51c0f6de208f346e952c6dff1
 
 	}
 
 	public Integer calc(String[] splitted, int i){
 		int op1=0;
 
-<<<<<<< HEAD
 		try{
 			op1 = mapaVariaveis.get(splitted[i]);
 				
 		}catch(NullPointerException b){
-=======
-        int result = 0;
-
-        /*switch (operador){
-            case '+': result = op1 + op2; break;
-            case '-': result = op1 - op2; break;
-            case '*': result = op1 * op2; break;
-            case '/': result = op1 / op2; break;
-            default: throws exception;
-        
-        }*/
-
-        //retorna o resultado
-        return result;
->>>>>>> c0eac72da8fb38b51c0f6de208f346e952c6dff1
 
 		try{
 			op1 = Integer.parseInt(splitted[i]);
 					
 		}catch(NumberFormatException e){
 			System.out.println("indefined value of"+splitted[i]);              
-			
+			return;
 			}
 		}
 
